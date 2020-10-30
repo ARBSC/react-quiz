5bf33713bc80773a083ab56f2ff8d97f2461774a
ariccardi <ariccardi@rezjam.com>
2020-10-30 00:41:30 -0400

    Initialize react app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component, Fragment } from 'react';
+import Navbar from './components/layout/Navbar';
+import './App.css';
+
+class App extends Component {
+  render () {
+    const name = 'Antonio';
+    return (
+      <Fragment>
+      <div className="App">
+        <Navbar title='Quiz App'/>
+    <h1>Hello from React, {name}</h1>
+      </div>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
